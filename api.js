821b34f0b46f2c3a8219d74c9cc02c5f560d0fad
juno-r-hyniva <juno.r@hyniva.com>
2023-10-13 16:47:29 +0530

    adding get and get all employee salary info

--- api.js
+++ api.js
@@ -106,14 +106,14 @@ const {
 } = require('@aws-sdk/client-dynamodb');
 
 // Importing the marshall function from the utility library for DynamoDB
-const { marshall } = require('@aws-sdk/util-dynamodb');
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
 // Creating an instance of DynamoDBClient
 const client = new DynamoDBClient();
 
 const handleDeleteOperation = async (event) => {
   const response = { statusCode: 200 };
-    const employeeId  = event.pathParameters.employeeId ;
+  const employeeId = event.pathParameters.employeeId;
   try {
     const employeeId = event.pathParameters.employeeId;
     const endpoint = event.path;
@@ -130,7 +130,7 @@ const handleDeleteOperation = async (event) => {
           Key: marshall({ employeeId: employeeId }),
           UpdateExpression: 'SET bankInfoDetails = :emptyList',
           ExpressionAttributeValues: {
-            ':emptyList': emptyList, 
+            ':emptyList': emptyList,
           },
           ConditionExpression: 'attribute_exists(employeeId )',
         };
@@ -146,7 +146,6 @@ const handleDeleteOperation = async (event) => {
         break;
 
       case `/employee/${employeeId}/softDelete`:
-        // Handle PATCH operation (Soft Delete)
         const updateExpression = 'SET bankInfoDetails[0].isActive = :isActive';
         const expressionAttributeValues = marshall({
           ':isActive': true,
@@ -180,7 +179,7 @@ const handleDeleteOperation = async (event) => {
     const metadata = e.$metadata || {};
     response.body = JSON.stringify({
       statusCode: metadata.httpStatusCode || response.statusCode,
-      message: `Failed to process${employeeId } employee operation.`,
+      message: `Failed to process${employeeId} employee operation.`,
       errorMsg: e.message,
       errorStack: e.stack,
     });
@@ -189,6 +188,116 @@ const handleDeleteOperation = async (event) => {
   return response;
 };
 
+// This program is for getting the employee details based http GET method.
+const {
+  GetItemCommand, // Retrieve data fron dynamoDb table
+  DynamoDBClient, // Dynamodb instance
+  ScanCommand,
+  UpdateItemCommand, //Scan the table
+} = require('@aws-sdk/client-dynamodb'); //aws-sdk is used to build rest APIs,
+//client-dynamodb library used to communicate with the
+
+//This function will get employee details based on empId
+//Create function as async with event as argument
+const handleGetEmployeeSalaryInfo = async (event) => {
+  //Initialize status code 200 OK
+  const response = { statusCode: 200 };
+  console.log(
+    'event data in request - ',
+    event.resource,
+    event.path,
+    event.headers.Accept,
+    event.httpMethod,
+    event.body
+  );
+  switch (`${event.resource} ${event.httpMethod}`) {
+    case '/employee/{empId}/salaryInfo GET':
+      const empId = event.pathParameters.empId;
+      //Try block code - this block evaluates the employee retrieve function based on empId,
+      // If true it gives employee details or it catches server response error and displayes at console
+      try {
+        // Define tablename and employeeId key with its value
+        const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          Key: marshall({ empId: empId }),
+          ProjectionExpression: 'empId, salaryInfo',
+        };
+        //Await response from client when sent GetItemCommand
+        //With params as argument containing tablename and key
+        const { Item } = await client.send(new GetItemCommand(params));
+        if (Item) {
+          //If item is present then send details
+          response.body = JSON.stringify({
+            message: `Successfully retrieved employee details of empId : ${empId}.`,
+            data: unmarshall(Item),
+          });
+        } else if (Item === undefined) {
+          //If Item is not found then send 404 error
+          response.statusCode = 404;
+          response.body = JSON.stringify({
+            message: `Employee details not found for empId : ${empId}.`,
+          });
+        } else {
+          response.statusCode = 500;
+          throw new Error(
+            `Unexpected error occurred while fetching empId : ${empId}.`
+          );
+        }
+      } catch (e) {
+        // Catch block to handle any errors
+        console.error(e);
+        response.body = JSON.stringify({
+          statusCode: response.statusCode,
+          message: `Failed to get employee details with empId : ${empId}.`,
+          errorMsg: e.message,
+          errorStack: e.stack,
+        });
+      }
+      break;
+
+    case '/employees/salaryInfo GET':
+      try {
+        const input = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          ProjectionExpression: 'empId, salaryInfo',
+        };
+        //Await response from client when sent scan command with tablename
+        const { Items } = await client.send(new ScanCommand(input));
+        if (Items.length === 0) {
+          // If employees are not present
+          response.statusCode = 404; // Setting the status code to 404
+          response.body = JSON.stringify({
+            message: 'Employees details are not found.',
+          });
+        } else {
+          // Generate response message and data
+          response.body = JSON.stringify({
+            message: 'Successfully retrieved all employees.',
+            data: Items?.map((item) => unmarshall(item)),
+          });
+        }
+      } catch (e) {
+        // Catch block to handle any server response errors
+        console.error(e);
+        response.body = JSON.stringify({
+          message: 'Failed to retrieve all employees.',
+          errorMsg: e.message,
+          errorStack: e.stack,
+        });
+      }
+      break;
+
+    default:
+      response.statusCode = 404;
+      response.body = JSON.stringify({
+        message: `URL not found -  ${event.resource}`,
+      });
+  }
+  //Return response with statusCode and data.
+  return response;
+};
+
 module.exports = {
+  handleGetEmployeeSalaryInfo,
   handleDeleteOperation,
 };
